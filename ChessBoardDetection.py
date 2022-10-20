6103085f1e89c4ef80fd85cb1020d38612fa957c
Pablo Pérez Martín <aperezvic21@gmail.com>
2022-10-20 16:59:46 +0200

    White Pawn detection

--- ChessBoardDetection.py
+++ ChessBoardDetection.py
@@ -9,19 +9,19 @@ import math
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 # Nombro la ventana
-cv.namedWindow("Cuadrados", cv.WINDOW_NORMAL)
+cv.namedWindow("Cuadrado", cv.WINDOW_NORMAL)
   
 # Reajustamos el tamaño de la ventana
-cv.resizeWindow("Cuadrados", 1000, 1000)
+cv.resizeWindow("Cuadrado", 1000, 1000)
 
-def showImage(img): # Funcion para mostrar la imagen
-    cv.imshow('Cuadrados',img)
+def showImage(name,img): # Funcion para mostrar la imagen
+    cv.imshow(name,img)
 
 #################################
 
 
 #Importamos imagen
-img = cv.imread('./test_images/test_image.png')
+img = cv.imread('./test_images/test_image.png',cv.IMREAD_UNCHANGED)
 #Sacamos Canny para los bordes porque si sacamos los contornos directamente no funciona por el ruido
 edges = cv.Canny(img,100,200)
 
@@ -55,11 +55,12 @@ for cnt in contours:
 img = cv.drawContours(img,[contornoMax],-1,(0,255,0),3)
 cv.putText(img,'Square',(x1,y1),cv.FONT_HERSHEY_SIMPLEX,0.6,(255,255,0),2)
 
+#TODO: cambiar nombre
 img = img[ySquare:ySquare+hSquare,xSquare:xSquare+wSquare] # Recorte de la imagen
 
 
 
-#showImage(img)
+#showImage("Cuadrado", img)
 
 
 square_edges = cv.Canny(img,100,200)
@@ -78,62 +79,48 @@ for f in range(8):
 
 print(squares_arr)
 
-for (x,y) in squares_arr:
+#TODO: Estructura de datos con el numero maximo de cada pieza
+#Empezamos solo detectando peones
+num_p = 0
+
+pawn_img = cv.imread('pawn.png',cv.IMREAD_UNCHANGED)
+peon_w = pawn_img.shape[1]
+peon_h = pawn_img.shape[0]
+
+#TODO: temporal
+threshold = 0.6
+
+def detectarPeon(cell,count): #FIXME: temporal
+    result = cv.matchTemplate(cell,pawn_img,cv.TM_CCOEFF_NORMED)
+    #if count == 55: 
+    #showImage("Celda" + str(count), result)
+    min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result)
+    print("PRECISION: " , max_val)
+    if max_val >= threshold: 
+        print("PEON")
+    
+
+#TODO: PONER NOMBRES DE VARIABLES REALES
+count = 0 #FIXME: temporal
+for (x,y) in squares_arr: #Recorremos los cuadrados pintando cada contorno
+    count = count+1
     top_left = (x,y)
     bottom_right = (x+square_size,y+square_size)
     cv.rectangle(img, top_left, bottom_right, color=(0, 255, 0), thickness=2, lineType=cv.LINE_4)
 
-showImage(img) # Enseñar bordes obtenidos con Canny
+    #Cropeamos las imagenes
+    chess_square = img[x:x+square_size,y:y+square_size]
+    detectarPeon(chess_square,count)
+    #showImage("Celda" + str(count), chess_square)
+
+
+showImage("Cuadrado", img) # Enseñar cuadrados con los contornos dibujados
+
+
+#Iteración de los cuadrados y detección de piezas
+
 
 
 cv.waitKey(0)
 cv.destroyAllWindows()
 
-
-# HOUGH
-""" lines = cv.HoughLinesP(edges, 1, np.pi/180, 60, np.array([]), 120, 5)
-print(lines)
-
-for line in lines:
-    for x1, y1, x2, y2 in line:
-        cv.line(img, (x1, y1), (x2, y2), (20, 220, 20), 3)
-
-# Naming a window
-cv.namedWindow("Lineas", cv.WINDOW_NORMAL)
-  
-# Using resizeWindow()
-cv.resizeWindow("Lineas", 1200, 700)
-
-cv.imshow('Lineas',img)
-cv.waitKey(0)
-cv.destroyAllWindows() """
-
-
-""" 
-gray_img = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
-
-#cv.imshow("Gray image",gray_img)
-
-#Binarización de la imagen
-ret,thresh = cv.threshold(gray_img,150,255,0)
-cv.imshow("Threshold",thresh)
-
-#Contornos
-contours, _ = cv.findContours(thresh,cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE)
-
-print("Numero de contornos: %i",len(contours))
-
-for cnt in contours:
-    x1,y1 = cnt[0][0]
-    approx = cv2.approxPolyDP(cnt,0.01*cv.arcLength(cnt,True),True)
-    if len(approx) == 4:
-        x, y, w, h = cv2.boundingRect(cnt)
-        ratio = float(w)/h
-        if 0.9 <= ratio <= 1.1:
-            img = cv.drawContours(img,[cnt],-1,(0,255,255),3)
-            cv.putText(img,'Square',(x1,y1),cv2.FONT_HERSHEY_SIMPLEX,0.6,(255,255,0),2)
-
-cv.imshow('Cuadrados',img)
-cv.waitKey(0)
-cv.destroyAllWindows()
- """
